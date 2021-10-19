668e6ead76930bfba1d9ebd2f7f9c51fea7ebb27
unique-chan <yechankim@gm.gist.ac.kr>
2021-10-19 16:45:24 +0900

    crowd_count v1

---
+++ cvpr16-mcnn/src/crowd_count.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from model import MCNN
+
+
+class CrowdCounter(nn.Module):
+    def __init__(self):
+        super(CrowdCounter, self).__init__()
+        self.DME = MCNN()
+        if torch.cuda.is_available():
+            self.DME.cuda()
+        self.loss_fn = nn.MSELoss()
+
+    @property
+    def loss(self):
+        return self.loss_mse
+
+    def forward(self, img, gt_density=None):
+        image = torch.from_numpy(img)
+        if torch.cuda.is_available():
+            image.cuda()
+        density_map = self.DME(image)
+
+        if self.training:
+            gt_density = torch.from_numpy(gt_density)
+            if torch.cuda.is_available():
+                gt_density.cuda()
+            self.loss_mse = self.loss_fn(density_map, gt_density)
+        return density_map
